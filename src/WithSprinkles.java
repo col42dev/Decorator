3840426b7dbee01c539f871851444024da82744e
Colin Moore <colin.moore42@googlemail.com>
2016-06-30 14:56:39 +0100

    initial commit

---
+++ src/WithSprinkles.java
@@ -0,0 +1,16 @@
+// Decorator WithSprinkles mixes sprinkles onto coffee.
+// Note it extends CoffeeDecorator.
+public class WithSprinkles extends CoffeeDecorator {
+	
+    public WithSprinkles(Coffee c) {
+        super(c);
+    }
+
+    public double getCost() {
+        return super.getCost() + 0.2;
+    }
+
+    public String getIngredients() {
+        return super.getIngredients() + ", Sprinkles";
+    }
+}
